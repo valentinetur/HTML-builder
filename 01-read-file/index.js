b4512c28a40ca4f8e43abf4d325de10379c7e65a
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-21 00:38:07 +0100

    fix read method from sync to async in 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-// const { stdin, stdout } = process;
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const filePath = path.join(__dirname, 'text.txt');
+const readSteam = fs.createReadStream(filePath, 'utf-8');
+
+readSteam.pipe(process.stdout);
