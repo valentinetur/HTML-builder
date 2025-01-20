db203d2b5aa47e81dbda33c406d8befd72bc8fb7
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-20 23:13:54 +0100

    feat: add 01-read-filed task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+// const { stdin, stdout } = process;
+
+fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
